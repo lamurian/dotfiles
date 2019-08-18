38b173ddddb28b655cf2c493b6e83906bc18aac9
lamurian <lamuri.aly8@gmail.com>
2019-08-18 03:16:01 +0100

    Using cat instead of acpi to get battery capacity. Change if condition to generate notification

--- bin/bat-stat.sh
+++ bin/bat-stat.sh
@@ -3,28 +3,16 @@
 # Check battery charge periodically to notify low and full charge
 bat_perc=$(cat /sys/class/power_supply/BAT0/capacity)
 bat_time=$(acpi -b | cut -d " " -f 5)
-status=$(cat /sys/class/power_supply/BAT0/status)
+stat=$(cat /sys/class/power_supply/BAT0/status)
 
-# Check whether display is available
-if [ -z $DISPLAY ]; then
-	if [ status=="discharging" ]; then
-		[ $bat_perc -lt 20 ] &&
-			tmux display-message \
-			"Plug the device, battery is $bat_perc"
-	else	# Notification if battery is charging
-		[ $bat_perc -gt 85 ] &&
-			tmux display-message \
-			"Unplug the device, battery is $bat_perc"
-	fi
-else 	# If display is available (xorg running)
-	if [ status=="discharging" ]; then
-		[ $bat_perc -lt 20 ] &&
-			notify-send --urgency critical "Plug the device" "$bat_time remaining" &&
-			mpg123 ~/.config/i3/ping-notif.mp3
-	else	# Notification if battery is charging
-		[ $bat_perc -gt 85 ] &&
-			notify-send --urgency normal "Unplug the device" "Battery is $bat_perc" &&
-			mpg123 ~/.config/i3/ping-notif.mp3
-	fi
+if [ $stat=="Discharging" ]; then
+	[ $bat_perc -lt 20 ] \
+	&& notify-send --urgency critical "Plug the device" "$bat_time remaining" \
+	&& mpg123 ~/.config/i3/ping-notif.mp3
+fi
 
+if [ $stat=="Charging" ]; then
+	[ $bat_perc -gt 85 ] \
+	&& notify-send --urgency normal "Unplug the device" "Battery is $bat_perc" \
+	&& mpg123 ~/.config/i3/ping-notif.mp3
 fi
