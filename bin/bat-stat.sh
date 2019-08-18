da9fa87f3afc32093bc104851db247d4a68021f2
lamurian <lamuri.aly8@gmail.com>
2019-08-18 03:25:31 +0100

    Minor correction

--- bin/bat-stat.sh
+++ bin/bat-stat.sh
@@ -5,13 +5,11 @@ bat_perc=$(cat /sys/class/power_supply/BAT0/capacity)
 bat_time=$(acpi -b | cut -d " " -f 5)
 stat=$(cat /sys/class/power_supply/BAT0/status)
 
-if [ $stat=="Discharging" ]; then
+if [ $stat = "Discharging" ]; then
 	[ $bat_perc -lt 20 ] \
 	&& notify-send --urgency critical "Plug the device" "$bat_time remaining" \
 	&& mpg123 ~/.config/i3/ping-notif.mp3
-fi
-
-if [ $stat=="Charging" ]; then
+else
 	[ $bat_perc -gt 85 ] \
 	&& notify-send --urgency normal "Unplug the device" "Battery is $bat_perc" \
 	&& mpg123 ~/.config/i3/ping-notif.mp3
