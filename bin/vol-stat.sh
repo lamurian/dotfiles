97ca7d03be78c35e6fdb20f6918f3ba7137e1ee8
lamurian <lamuri.aly8@gmail.com>
2019-10-07 22:26:53 +0100

    Change icon

--- bin/vol-stat.sh
+++ bin/vol-stat.sh
@@ -5,14 +5,9 @@
 vol=$(amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1)
 
 # Decide whether the volume is muted or not
-if amixer get Master \
-	| tail -n 1 \
-	| cut -d '[' -f 4 | cut -d ']' -f 1 \
-	| grep off > /dev/null; then
-	icon=""
-else
+amixer get Master | tail -n 1 | grep off 2>&1 > /dev/null && \
+	icon="<span foreground='#666666'></span>" || \
 	icon=""
-fi
 
 md1o="<span foreground='#999999'><b>"	# Open tag for markdown formatting
 md1c="</b></span>"			# Close tag for markdown formatting
@@ -22,37 +17,37 @@ md2c="</span>"
 # Make volume slider
 case $vol in
 	0)
-		level="$md2o――――――――――――――――――――$md2c"
+		level="$md2o────────────────────$md2c"
 		;;
 	[1-9])
-		level="$md1o――$md1c$md2o――――――――――――――――――$md2c"
+		level="$md1o──$md1c$md2o──────────────────$md2c"
 		;;
 	1[0-9])
-		level="$md1o――――$md1c$md2o――――――――――――――――$md2c"
+		level="$md1o────$md1c$md2o────────────────$md2c"
 		;;
 	2[0-9])
-		level="$md1o――――――$md1c$md2o――――――――――――――$md2c"
+		level="$md1o──────$md1c$md2o──────────────$md2c"
 		;;
 	3[0-9])
-		level="$md1o――――――――$md1c$md2o――――――――――――$md2c"
+		level="$md1o────────$md1c$md2o────────────$md2c"
 		;;
 	4[0-9])
-		level="$md1o――――――――――$md1c$md2o――――――――――$md2c"
+		level="$md1o──────────$md1c$md2o──────────$md2c"
 		;;
 	5[0-9])
-		level="$md1o――――――――――――$md1c$md2o――――――――$md2c"
+		level="$md1o────────────$md1c$md2o────────$md2c"
 		;;
 	6[0-9])
-		level="$md1o――――――――――――――$md1c$md2o――――――$md2c"
+		level="$md1o──────────────$md1c$md2o──────$md2c"
 		;;
 	7[0-9])
-		level="$md1o――――――――――――――――$md1c$md2o――――$md2c"
+		level="$md1o────────────────$md1c$md2o────$md2c"
 		;;
 	8[0-9])
-		level="$md1o――――――――――――――――――$md1c$md2o――$md2c"
+		level="$md1o──────────────────$md1c$md2o──$md2c"
 		;;
 	*)
-		level="$md1o――――――――――――――――――――$md1c"
+		level="$md1o────────────────────$md1c"
 		;;
 esac
 
@@ -60,4 +55,4 @@ esac
 dunstify -r 1 -u low -t 800 "Volume" \
 "<span font='Font Awesome 10'>$icon</span> \
 <span font='Inconsolata 10'>$vol</span> \
-<span font='Font Awesome 7'>$level</span>"
+<span font='Font Awesome 5'>$level</span>"
