cbada999197182ec41ead1cb5dc410b07dd16fc4
lamurian <lamuri.aly8@gmail.com>
2019-11-09 13:13:02 +0000

    Revert backlight and volume status to master config (somehow the glyph work now)

--- bin/bklight-stat.sh
+++ bin/bklight-stat.sh
@@ -12,70 +12,71 @@ md2c="</span>"
 
 case $lglevel in
 	[0-4])
-		stat="$md2o--------------------$md2c"
+		stat="$md2o────────────────────$md2c"
 		;;
 	[5-9])
-		stat="$md1o-$md1c$md2o-------------------$md2c"
+		stat="$md1o─$md1c$md2o───────────────────$md2c"
 		;;
 	1[0-4])
-		stat="$md1o--$md1c$md2o------------------$md2c"
+		stat="$md1o──$md1c$md2o──────────────────$md2c"
 		;;
 	1[5-9])
-		stat="$md1o---$md1c$md2o-----------------$md2c"
+		stat="$md1o───$md1c$md2o─────────────────$md2c"
 		;;
 	2[0-4])
-		stat="$md1o----$md1c$md2o----------------$md2c"
+		stat="$md1o────$md1c$md2o────────────────$md2c"
 		;;
 	2[5-9])
-		stat="$md1o-----$md1c$md2o---------------$md2c"
+		stat="$md1o─────$md1c$md2o───────────────$md2c"
 		;;
 	3[0-4])
-		stat="$md1o------$md1c$md2o--------------$md2c"
+		stat="$md1o──────$md1c$md2o──────────────$md2c"
 		;;
 	3[5-9])
-		stat="$md1o-------$md1c$md2o-------------$md2c"
+		stat="$md1o───────$md1c$md2o─────────────$md2c"
 		;;
 	4[0-4])
-		stat="$md1o--------$md1c$md2o------------$md2c"
+		stat="$md1o────────$md1c$md2o────────────$md2c"
 		;;
 	4[5-9])
-		stat="$md1o---------$md1c$md2o-----------$md2c"
+		stat="$md1o─────────$md1c$md2o───────────$md2c"
 		;;
 	5[0-4])
-		stat="$md1o----------$md1c$md2o----------$md2c"
+		stat="$md1o──────────$md1c$md2o──────────$md2c"
 		;;
 	5[5-9])
-		stat="$md1o-----------$md1c$md2o---------$md2c"
+		stat="$md1o───────────$md1c$md2o─────────$md2c"
 		;;
 	6[0-4])
-		stat="$md1o------------$md1c$md2o--------$md2c"
+		stat="$md1o────────────$md1c$md2o────────$md2c"
 		;;
 	6[5-9])
-		stat="$md1o-------------$md1c$md2o-------$md2c"
+		stat="$md1o─────────────$md1c$md2o───────$md2c"
 		;;
 	7[0-4])
-		stat="$md1o--------------$md1c$md2o------$md2c"
+		stat="$md1o──────────────$md1c$md2o──────$md2c"
 		;;
 	7[5-9])
-		stat="$md1o---------------$md1c$md2o-----$md2c"
+		stat="$md1o───────────────$md1c$md2o─────$md2c"
 		;;
 	8[0-4])
-		stat="$md1o----------------$md1c$md2o----$md2c"
+		stat="$md1o────────────────$md1c$md2o────$md2c"
 		;;
 	8[5-9])
-		stat="$md1o-----------------$md1c$md2o---$md2c"
+		stat="$md1o─────────────────$md1c$md2o───$md2c"
 		;;
 	9[0-4])
-		stat="$md1o------------------$md1c$md2o--$md2c"
+		stat="$md1o──────────────────$md1c$md2o──$md2c"
 		;;
 	9[5-9])
-		stat="$md1o-------------------$md1c$md20-$md2c"
+		stat="$md1o───────────────────$md1c$md20─$md2c"
 		;;
 	*)
-		stat="$md1o--------------------$md1c"
+		stat="$md1o────────────────────$md1c"
 		;;
 esac
 
 dunstify -r 1 -u low "Backlight" \
 "<span font='Font Awesome 8'>$icon</span> \
-<span font='Inconsolata 10'>$lglevel $stat</span>"
+<span font='Inconsolata 10'>$lglevel</span> \
+<span font='Font Awesome 5'>$stat</span>"
