8ca54afda6e51b3e0e0b056a54cb961f87bb1e7c
lamurian <lamuri.aly8@gmail.com>
2020-04-09 20:43:39 +0700

    Finalizing omnimenu

--- bin/scast.sh
+++ bin/scast.sh
@@ -2,15 +2,15 @@
 
 # Little script to produce screen cast
 
-OUTPUT=${1}
+[ -z $1 ] && OUTPUT=$(date +%y%m%d_%H%M%S.mkv) || OUTPUT=${1}
 SIZE=$(xrandr -q --current | grep \* | awk '{print $1}')
 SOUND=$(pactl list sources short | awk '{print $2}' | grep mon)
 
-ffmpeg -f alsa \
-	-i pulse \
-	-f x11grab \
+ffmpeg \
+        -f pulse -i default \
+        -f x11grab \
 	-video_size $SIZE \
-	-framerate 25 \
-	-i :0.0 \
+        -framerate 25 \
+        -i :0.0 \
 	-c:v libx264 -c:a aac \
 	$OUTPUT
