9924d55dc754bc21e28b2538b16b459292aa5950
lamurian <lamuri.aly8@gmail.com>
2020-09-13 00:01:48 +0700

    Cleaning code and updating newsboat database

--- bin/scast.sh
+++ bin/scast.sh
@@ -2,15 +2,14 @@
 
 # Little script to produce screen cast
 
-[ -z $1 ] && OUTPUT=$(date +%y%m%d_%H%M%S.mkv) || OUTPUT=${1}
-SIZE=$(xrandr -q --current | grep \* | awk '{print $1}')
+[ -z $1 ] && OUTPUT=$(date +%y%m%d_%H%M%S.mp4) || OUTPUT=${1}
+SIZE=$(xrandr -q --current | grep \* | awk '{print $1}' | head -n 1)
 SOUND=$(pactl list sources short | awk '{print $2}' | grep mon)
 
 ffmpeg \
         -f pulse -i default \
         -f x11grab \
 	-video_size $SIZE \
-        -framerate 25 \
-        -i :0.0 \
-	-c:v libx264 -c:a aac \
-	$OUTPUT
+        -framerate 30 \
+        -i :0.0 -c:v libx264 \
+	-f mp4 $OUTPUT
